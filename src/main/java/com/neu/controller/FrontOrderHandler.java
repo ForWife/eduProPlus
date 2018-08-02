2ac52779a25dac0951f98c763b3432b832a76b1d
WeiXiang <wojiaoweixiang@qq.com>
2018-08-02 21:43:12 +0800

    order

--- src/main/java/com/neu/controller/FrontOrderHandler.java
+++ src/main/java/com/neu/controller/FrontOrderHandler.java
@@ -85,12 +85,5 @@ public class FrontOrderHandler
 		  System.out.println("....servlet....cancel()......");
 		  return frontorderService.cancel(oid);
       }
-//	  add Order
-	  @RequestMapping(value="/front/addUser")
-	  @ResponseBody
-	  public String addOrder(int lid,int branchid,String nickname,String tel){
-		  
-		  return "";
-		  
-	  }
+
 }
